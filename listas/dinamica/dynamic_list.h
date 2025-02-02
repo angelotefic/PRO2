c92041a989692def3ce8c0b108f3fbebfa609a41
angelcanosa <114648265+angelotefic@users.noreply.github.com>
2025-02-02 20:03:30 +0100

    Rename dynamic_list.h

---
+++ listas/dinamica/dynamic_list.h
@@ -0,0 +1,32 @@
+#ifndef DYNAMIC_LIST_H
+#define DYNAMIC_LIST_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define LNULL NULL
+
+typedef int tItemL;
+
+typedef struct tNode {
+    tItemL data;
+    struct tNode *next;
+} tNode;
+
+typedef tNode* tPosL;
+typedef tPosL tList;
+
+void createEmptyList(tList *L);
+bool isEmptyList(tList L);
+bool createNode(tPosL *p);
+bool insertItem(tItemL d, tPosL p, tList *L);
+void updateItem(tItemL d, tPosL p, tList *L);
+tPosL findItem(tItemL d, tList L);
+tItemL getItem(tPosL p, tList L);
+tPosL first(tList L);
+tPosL last(tList L);
+tPosL previous(tPosL p, tList L);
+tPosL next(tPosL p, tList L);
+void deleteAtPosition(tPosL p, tList *L);
+
+#endif // DYNAMIC_LIST_H
